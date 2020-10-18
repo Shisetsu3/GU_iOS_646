6963971e0df05f89caa2594ec98d4af43e798a1f
Shisetsu <shisetsu@icloud.com>
2020-10-18 17:10:44 +0300

    Исправления домашних работ по 6 и 7 уроку.

--- 7I_ФИ.playground/Contents.swift
+++ 7I_ФИ.playground/Contents.swift
@@ -3,52 +3,10 @@ struct Games {
     var count: Int
     var name: Title
 }
-import Cocoa
 
 struct Title {
     let name: String
 }
-
-class GameShop {
-    var stock = [
-        "Doom Eternal": Games(price: 2330, count: 7, name: Title(name: "Doom Eternal")),
-        "Alien": Games(price: 1432, count: 0, name: Title(name: "Alien")),
-        "Wolfenstein": Games(price: 5400, count: 3, name: Title(name: "Wolfenstein")),
-        "WRC 8": Games(price: 2435, count: 2, name: Title(name: "WRC 8")),
-        "NFS": Games(price: 3800, count: 6, name: Title(name: "NFS"))
-    ]
-    
-    var fundsLimit: Double = 1500
-    
-    func buy(game name: String) -> Title? {
-        guard let buyingGame = stock[name] else {
-            return nil
-        }
-        
-        guard stock.count > 0 else {
-            return nil
-        }
-        
-        guard buyingGame.price <= fundsLimit else {
-            return nil
-        }
-        
-        fundsLimit -= buyingGame.price
-        var newItem = buyingGame
-        newItem.count -= 1
-        stock[name] = newItem
-        print("Вы приобрели игру: \(name)")
-        print("У вас на счету осталось: \(fundsLimit) руб.")
-        return newItem.name
-    }
-}
-
-let gameShop = GameShop()
-//gameShop.buy(game: "NFS")
-//gameShop.buy(game: "Doom Eternal")
-//gameShop.buy(game: "Alien")
-
-//2 часть
 
 enum GameShopError: Error {
 
@@ -66,6 +24,63 @@ extension GameShopError: CustomStringConvertible {
         }
     }
 }
+
+class GameShop {
+    var stock = [
+        "Doom Eternal": Games(price: 2330, count: 7, name: Title(name: "Doom Eternal")),
+        "Alien": Games(price: 1432, count: 0, name: Title(name: "Alien")),
+        "Wolfenstein": Games(price: 5400, count: 3, name: Title(name: "Wolfenstein")),
+        "WRC 8": Games(price: 2435, count: 2, name: Title(name: "WRC 8")),
+        "NFS": Games(price: 3800, count: 6, name: Title(name: "NFS"))
+    ]
+    
+    var fundsLimit: Double = 1500
+    
+    func buy(game name: String) -> (game: Title?, error: GameShopError?){
+        guard let buyingGame = stock[name] else {
+            return (game: nil, error: GameShopError.wrongName)
+        }
+        
+        guard stock.count > 0 else {
+            return (game: nil, error: GameShopError.outOfStock)
+        }
+        
+        guard buyingGame.price <= fundsLimit else {
+            return (game: nil, error: GameShopError.noCredit(credit: buyingGame.price - fundsLimit))
+        }
+        
+        fundsLimit -= buyingGame.price
+        var newItem = buyingGame
+        newItem.count -= 1
+        stock[name] = newItem
+        return (game: newItem.name, error: nil)
+    }
+}
+
+let gameShop = GameShop()
+//let buy1 = gameShop.buy(game: "NFS")
+//let buy2 = gameShop.buy(game: "Doom Eternal")
+//let buy3 = gameShop.buy(game: "Alien")
+
+if let product = buy1.game {
+    print("Мы купили: \(product.name)")
+} else if let error = buy1.error {
+    print("Произошла ошибка: \(error.description)")
+}
+
+if let product = buy2.game {
+    print("Мы купили: \(product.name)")
+} else if let error = buy2.error {
+    print("Произошла ошибка: \(error.description)")
+}
+
+if let product = buy3.game {
+    print("Мы купили: \(product.name)")
+} else if let error = buy3.error {
+    print("Произошла ошибка: \(error.description)")
+}
+
+//2 часть
 
 extension GameShop {
     
